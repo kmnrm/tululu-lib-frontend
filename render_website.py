c06433cdc7b8b9b89f22ebf3ceecdb7e8a6e4aa9
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-06-13 21:13:08 +0800

    Immediately update changes for index.html

--- render_website.py
+++ render_website.py
@@ -1,6 +1,18 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+
+def on_reload():
+    template = env.get_template('template.html')
+    rendered_page = template.render(books=books)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 
 class Book(object):
@@ -25,19 +37,8 @@ books = [
     for book in parsed_books
 ]
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+on_reload()
 
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    books=books
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+server = Server()
+server.watch('template.html', on_reload)
+server.serve(root='.')
