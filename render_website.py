b8f7daa2be5267c2dc8aeec383deaa90e6944ced
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-07-11 22:37:55 +0800

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import os
 import json
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -11,9 +12,11 @@ env = Environment(
 
 def on_reload():
     template = env.get_template('template.html')
-    rendered_page = template.render(books=books, books_pairs=books_pairs)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    os.makedirs('pages', exist_ok=True)
+    for page_num, page_books_pairs in enumerate(books_pairs_split):
+        rendered_page = template.render(books=books, books_pairs=page_books_pairs)
+        with open(f'pages/index{page_num + 1}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 class Book(object):
@@ -30,15 +33,21 @@ parsed_books = json.loads(parsed_books)
 
 books = [
     Book(
-        '/'.join(map(str, book["book_path"].split('\\')[-2:])),
-        '/'.join(map(str, book["image_src"].split('\\')[-2:])),
+        '../{path}'.format(
+            path='/'.join(map(str, book["book_path"].split('\\')[-2:]))
+        ),
+        '../{path}'.format(
+            path='/'.join(map(str, book["image_src"].split('\\')[-2:]))
+        ),
         book["title"],
         book["author"]
     )
     for book in parsed_books
 ]
 
+pairs_divider = 10
 books_pairs = list(chunked(books, 2))
+books_pairs_split = list(chunked(books_pairs, pairs_divider))
 
 on_reload()
 
